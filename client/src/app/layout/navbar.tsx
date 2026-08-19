0c59a86d8b7ed4f930c5b6c87f082d2b6d3f2e7f
NojusZeimys <113110331+NojusZeimys@users.noreply.github.com>
2026-08-19 23:41:34 +0300

    end of section 5

---
+++ client/src/app/layout/navbar.tsx
@@ -0,0 +1,35 @@
+import { Group, Menu } from "@mui/icons-material";
+import { AppBar, Box, Button, Container, IconButton, MenuItem, MenuList, Toolbar, Typography } from "@mui/material";
+
+type Props = {
+  openForm: () => void;
+};
+
+export default function NavBar({ openForm }: Props) {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{ backgroundImage: "linear-gradient(135deg,#182a73 0%, #218aae 69%, #20a7ac 89%)" }}>
+        <Container maxWidth="xl">
+          <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+            <MenuList>
+              <MenuItem sx={{ display: "flex", gap: 2 }}>
+                <Group fontSize="large" />
+                <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+                  Reactivities
+                </Typography>
+              </MenuItem>
+            </MenuList>
+            <MenuList sx={{ display: "flex" }}>
+              <MenuItem sx={{ fontSize: "1.2rem", textTransform: "uppercase", fontWeight: "bold" }}>activities</MenuItem>
+              <MenuItem sx={{ fontSize: "1.2rem", textTransform: "uppercase", fontWeight: "bold" }}>about</MenuItem>
+              <MenuItem sx={{ fontSize: "1.2rem", textTransform: "uppercase", fontWeight: "bold" }}>contact</MenuItem>
+            </MenuList>
+            <Button size="large" variant="contained" color="warning" onClick={openForm}>
+              Create activity
+            </Button>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </Box>
+  );
+}
