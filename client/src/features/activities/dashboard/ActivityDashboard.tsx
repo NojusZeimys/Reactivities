0dae7e8f8e2ff74b4246f645606a6f36c1c3d2c7
NojusZeimys <113110331+NojusZeimys@users.noreply.github.com>
2026-08-24 15:39:48 +0300

    End of section 8

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,15 +1,16 @@
 import { Grid } from "@mui/material";
 import ActivityList from "./ActivityList";
-import ActivityDetails from "../details/ActivityDetails";
-import ActivityForm from "../form/ActivityForm";
+import ActivityFilters from "./ActivityFilters";
 
 export default function ActivityDashboard() {
   return (
     <Grid container spacing={3}>
-      <Grid size={7}>
+      <Grid size={8}>
         <ActivityList></ActivityList>
       </Grid>
-      <Grid size={5}>Activity filters go here</Grid>
+      <Grid size={4}>
+        <ActivityFilters />
+      </Grid>
     </Grid>
   );
 }
--- client/src/features/activities/dashboard/ActivityList.tsx
+++ client/src/features/activities/dashboard/ActivityList.tsx
@@ -1,5 +1,4 @@
 import { Box, Typography } from "@mui/material";
-import React from "react";
 import ActivityCard from "./ActivityCard";
 import { useActivities } from "../../../lib/hooks/useActivities";
 
